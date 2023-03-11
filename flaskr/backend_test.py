dab1bdb6dfe0efb3238cc575b91011092fe481c7
Barsha  Chaudhary <barshachaudhary@techexchange.in>
2023-03-11 01:54:16 +0000

    added testing for sign_in and user_info

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -86,32 +86,47 @@ def test_get_wiki_page_fail(file_stream, blob, bucket, storage_client):
     bucket.get_blob.open.readlines.assert_not_called()
     assert not text
 
-#Testing for sign_up() and sign_in_fail() method starts from here:
+"""
+Testing for sign_up(), sign_in(), hash_password() and test_get_user_info() method starts from here:
+I have used fixture to set expected output and input value that I get from the bucket.
+"""
+
+#json_value that will be stored inside the bucket if each file to store user_info
 @pytest.fixture
 def user_info():    
     return {"username": "barsha@gmail.com", "password": "pa$$word", "first_name": "Barsha", "last_name": "Chaudhary"}
 
+#username from the json that is passed to the bucket
 @pytest.fixture
+#will be used in sign_up_test
 def user_name(user_info):                                   
     return user_info["username"]
 
+#user_password from the json that is passed to the bucket
+#will be used in sign_up_test
 @pytest.fixture
 def user_password(user_info):
     return user_info["password"]
 
+#user_first_name from the json that is passed to the bucket
+#will be used in sign_up_test
 @pytest.fixture
 def user_first_name(user_info):
     return user_info["first_name"]
 
+#user_last_name from the json that is passed to the bucket
+#will be used in sign_up_test
 @pytest.fixture
 def user_last_name(user_info):
     return user_info["last_name"]
 
-def test_sign_up_registered_user(storage_client,blob,bucket,user_name,user_password,user_first_name,user_last_name):
+#test for the sign_up when user email is already taken
+def test_sign_up(storage_client,blob,bucket,user_name,user_password,user_first_name,user_last_name):
     b = Backend(storage_client)
     output = b.sign_up(user_name,user_password,user_first_name,user_last_name)
     assert output == "Username Taken!"
-
+    
+#test for sign for the new users who are yet to register
 def test_sign_up_new_user(storage_client,blob,bucket,user_name,user_password,user_first_name,user_last_name,user_info):
     with patch('google.cloud.storage.Blob') as storage_blob:
         storage_blob.return_value.exists.return_value = False
@@ -120,14 +135,46 @@ def test_sign_up_new_user(storage_client,blob,bucket,user_name,user_password,use
         bucket.blob.assert_called_with(user_name + ".txt")
         blob.upload_from_string.assert_called_with(json.dumps(user_info))
 
+#testing for the hash_password() 
 def test_hash_password(storage_client, user_password):
     b = Backend(storage_client)
     hashed_password = b.hash_password(user_password)
     assert hashed_password == '4b787f1b2774c29601d145fb70becb6c0d507a624eca769d1e85bc5ffbf33ed89a1c5f848444a286f5f639412129441ff58a0d657cbd56423d02c6d97985494a'     
 
+#testing when the sign in fails
 def test_sign_in_fail(storage_client,blob,bucket,user_name,user_password):
     with patch('google.cloud.storage.Blob') as storage_blob:
             storage_blob.return_value.exists.return_value = False
             b = Backend(storage_client)
             check_sign_in = b.sign_in(user_name,user_password)
             assert check_sign_in == False
+
+#fixture that returns the data that is expected from the files inside the bucket
+#blobs are storing the file content in the string form as the files are of txt format 
+#used in test_sign_in and test_user_info
+@pytest.fixture
+def user_info_json_loads():
+    stored_info = '{"username": "barsha@gmail.com", "password": "pa$$word", "first_name": "Barsha", "last_name": "Chaudhary"}'
+    return stored_info
+
+#testing for the sucessful login when the password entered matches
+def test_sign_in_success(storage_client,blob,bucket,user_name,user_password,user_info_json_loads,user_info):
+    with patch('google.cloud.storage.Blob') as storage_blob:
+        storage_blob.return_value.exists.return_value = True
+        blob.download_as_text.return_value = user_info_json_loads
+        b = Backend(storage_client)
+        b.sign_in(user_name,user_password)
+        bucket.blob.assert_called_with(user_name + ".txt")
+        json_output = json.loads(blob.download_as_text())
+        assert user_info["password"] == json_output["password"]
+
+#testing to get general info [firstname and lastname] about the user 
+def test_get_user_info(storage_client,blob,bucket,user_name,user_password,user_info_json_loads,user_info):
+    blob.download_as_text.return_value = user_info_json_loads
+    json_output = json.loads(blob.download_as_text())
+    b = Backend(storage_client)
+    output = b.get_user_info(user_name)
+    bucket.blob.assert_called_with(user_name + ".txt")
+    assert json_output["first_name"] == output["Firstname"]
+    assert json_output["last_name"] == output["Secondname"]
+    assert output == {"Firstname": "Barsha","Secondname": "Chaudhary"}
