2f8a2e2609a2144bf663473299d77c8ebccd441c
Barsha  Chaudhary <barshachaudhary@techexchange.in>
2023-04-10 06:09:05 +0000

    changes to the app.yaml

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,7 +16,18 @@ def create_app(test_config=None):
 
     # This is the default secret key used for login sessions
     # By default the dev environment uses the key 'dev'
-    app.config.from_mapping(SECRET_KEY='dev',)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    # app.config['MAIL_USE_TLS'] = True
+    #app.config['SERVER_NAME'] = 'wiki-viewer-377817.uc.r.appspot.com'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['MAIL_USERNAME'] = 'barsha.chaudhary@bison.howard.edu'
+    app.config['MAIL_PASSWORD'] = 'meqlzeaggmlydozu'
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing.
